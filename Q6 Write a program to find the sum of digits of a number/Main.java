576823de29cb0fa3fb95cc868e5baec9308c0bf5
developermickey <mukesh.quazma@gmail.com>
2024-09-23 15:51:41 +0530

    Write a program to find the sum of digits of a number

---
+++ Q6 Write a program to find the sum of digits of a number/Main.java
@@ -0,0 +1,17 @@
+//Write a program to find the sum of digits of a number;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int sum = 0;
+        while (n > 0) {
+            sum += n % 10;
+            n /= 10;
+        }
+        System.out.println("Sum of digits: " + sum);
+    }
+}
