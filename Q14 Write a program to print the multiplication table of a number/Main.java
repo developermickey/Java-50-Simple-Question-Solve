e3407436ee23955b611a70f9c70df0fe93804d73
developermickey <mukesh.quazma@gmail.com>
2024-09-24 13:55:52 +0530

    Write a program to print the multiplication table of a number

---
+++ Q14 Write a program to print the multiplication table of a number/Main.java
@@ -0,0 +1,12 @@
+//  Write a program to print the multiplication table of a number;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int n = 10;
+        for (int i = 1; i <= n; i++) {
+            System.err.println("3 x " + i + " = " + i * 3);
+        }
+
+    }
+}
