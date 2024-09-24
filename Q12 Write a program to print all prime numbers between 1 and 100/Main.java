9ce6621c6a01070dbdfad1312c341b9349ab83b7
developermickey <mukesh.quazma@gmail.com>
2024-09-24 13:07:11 +0530

    Prime Number Code Update

--- Q12 Write a program to print all prime numbers between 1 and 100/Main.java
+++ Q12 Write a program to print all prime numbers between 1 and 100/Main.java
@@ -1,28 +1,32 @@
+
 public class Main {
 
-    // Method to check if a number is prime
-    static boolean isPrime(int n) {
-        if (n <= 1) {
-            return false; // Numbers less than or equal to 1 are not prime
+    // check number is prime or not
+
+    static boolean isPrime(int a) {
+        if (a <= 1) {
+            return false;
         }
-        for (int i = 2; i <= Math.sqrt(n); i++) {
-            if (n % i == 0) {
-                return false; // If divisible by any number other than 1 and itself, it's not prime
+
+        for (int i = 2; i <= Math.sqrt(a); i++) {
+            if (a % i == 0) {
+                return false;
             }
         }
         return true;
     }
 
-    // Method to print prime numbers between 1 and a given limit
-    static void printPrimeNumbers(int limit) {
-        for (int i = 2; i <= limit; i++) {
+    // print prine number
+
+    static void printPrimeNumber(int num) {
+        for (int i = 2; i <= num; i++) {
             if (isPrime(i)) {
-                System.out.println(i); // Print the prime number
+                System.out.println(i);
             }
         }
     }
 
     public static void main(String[] args) {
-        printPrimeNumbers(100); // Print all prime numbers between 1 and 100
+        printPrimeNumber(100);
     }
 }
