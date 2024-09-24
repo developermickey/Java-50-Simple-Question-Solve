ec54013ec245db7239350e76a2db8e05610e1e8f
developermickey <mukesh.quazma@gmail.com>
2024-09-24 15:33:53 +0530

    Write a program to print the following pattern

---
+++ Q21 Write a program to print the following pattern/Main.java
@@ -0,0 +1,26 @@
+// Write a program to print the following pattern;
+/* 
+*
+**
+***
+****
+*****
+
+*/
+
+public class Main {
+    public static void main(String[] args) {
+        int n = 5;
+
+        // outer loop
+
+        for (int i = 1; i <= n; i++) {
+            // inner loop
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+
+        }
+    }
+}
