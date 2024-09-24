864cafeacafcb927457c10a3ca7e136788ad0d9f
developermickey <mukesh.quazma@gmail.com>
2024-09-24 15:38:50 +0530

    2nd Pattern Added

---
+++ Q22 Write a program to print the following pattern/Main.java
@@ -0,0 +1,25 @@
+// Write a program to print the following pattern;
+
+/*
+
+*****
+****
+***
+**
+*
+
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        int n = 5;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i + 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+
+        }
+    }
+}
