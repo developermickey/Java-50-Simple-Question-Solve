0e83d81b3202d7f21ae5d6100428e0eac208ade1
developermickey <mukesh.quazma@gmail.com>
2024-09-24 14:08:14 +0530

    Write a program to print numbers in reverse order from N to 1

---
+++ Q18 Write a program to print numbers in reverse order from N to 1/Main.java
@@ -0,0 +1,11 @@
+// Write a program to print numbers in reverse order from N to 1;
+
+public class Main {
+    public static void main(String[] args) {
+        int n = 10;
+
+        for (int i = n; i >= 1; i--) {
+            System.out.println(i);
+        }
+    }
+}
